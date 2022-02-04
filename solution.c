1130c0b64f59b5241435501277d3f2f509072313
Tertle950 <tertle950@gmail.com>
2022-02-04 15:14:21 +0000

    commit on main

--- solution.c
+++ solution.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    //
+    int ARR_LENGTH = 0;
+    printf("Width & length of the square? ");
+    scanf("%i",&ARR_LENGTH);
+
+    printf("Square is %ix%i\n",ARR_LENGTH,ARR_LENGTH);
 }
