972483ecf53e7029d5b0f4efec80ab57035a59db
Tertle950 <tertle950@gmail.com>
2022-02-09 15:56:47 +0000

    I'm still not done...

--- solution.c
+++ solution.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 void printAbacus(bool *ARR, int ARRSIZE);
 int latestAbacus(bool *ARR, int ARRSIZE);
-bool moveAbacus(bool *ARR, int ARRSIZE);
+int moveAbacus(bool *ARR, int ARRSIZE);
 
 int main(){
     int SQUARESIZE = 0;
@@ -16,12 +16,19 @@ int main(){
     }
     
     int count = 1;
+    int try = 1;
     printAbacus(ARR, ARRSIZE);
-    while(!moveAbacus(ARR, ARRSIZE)){
+    while(true){
+        printf("count = %i | ",count);
+        try = moveAbacus(ARR, ARRSIZE);
         printAbacus(ARR, ARRSIZE);
-        count++;
+        if(try == -1){
+            break;
+        }else{
+            count += try;
+        }
+        
     }
-    //printAbacus(ARR, ARRSIZE);
     
     printf("%i possibilities found\n",count);
     return 0;
@@ -41,9 +48,11 @@ int latestAbacus(bool *ARR, int ARRSIZE){
     return -1;
 }
 
-bool moveAbacus(bool *ARR, int ARRSIZE){
+int moveAbacus(bool *ARR, int ARRSIZE){
     int i = latestAbacus(ARR, ARRSIZE);
     int j = 0;
+
+    printf("i = %i | ",i);
 
     if(i == ARRSIZE - 1){
         while(ARR[i]){
@@ -54,7 +63,9 @@ bool moveAbacus(bool *ARR, int ARRSIZE){
 
         i = latestAbacus(ARR, ARRSIZE);
         if(i == -1){
-            return true;
+            //printAbacus(ARR, ARRSIZE);
+            return -1;
+            // End of the line
         }
         if(i < 0){
             printf("Left Outside of array error!\n");
@@ -70,11 +81,10 @@ bool moveAbacus(bool *ARR, int ARRSIZE){
             }
             ARR[i] = true;
         }
-
+        return 1;
     }else{
         ARR[i] = false;
-        ARR[i + 1] = true;
+        ARR[ARRSIZE - 1] = true;
+        return ARRSIZE - i - 1;
     }
-
-    return false; // UNLESS abacus cannot be moved any longer!
 }
