595db1ce574dc894b4d503a75f7b00457a5387ce
Tertle950 <tertle950@gmail.com>
2022-02-09 16:10:24 +0000

    it takes too long...

--- solution.c
+++ solution.c
@@ -19,9 +19,9 @@ int main(){
     int try = 1;
     printAbacus(ARR, ARRSIZE);
     while(true){
-        printf("count = %i | ",count);
+        //printf("count = %i | ",count);
         try = moveAbacus(ARR, ARRSIZE);
-        printAbacus(ARR, ARRSIZE);
+        //printAbacus(ARR, ARRSIZE);
         if(try == -1){
             break;
         }else{
@@ -52,7 +52,7 @@ int moveAbacus(bool *ARR, int ARRSIZE){
     int i = latestAbacus(ARR, ARRSIZE);
     int j = 0;
 
-    printf("i = %i | ",i);
+    //printf("i = %i | ",i);
 
     if(i == ARRSIZE - 1){
         while(ARR[i]){
@@ -67,18 +67,22 @@ int moveAbacus(bool *ARR, int ARRSIZE){
             return -1;
             // End of the line
         }
+        /*
         if(i < 0){
             printf("Left Outside of array error!\n");
             i = 0;
         }
+        */
         ARR[i] = false;
         
         for(; j >= 0; j--){
             i++;
+            /*
             if(i >= ARRSIZE){
                 printf("Right Outside of array error!\n");
                 i = ARRSIZE - 1;
             }
+            */
             ARR[i] = true;
         }
         return 1;
